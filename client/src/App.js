753de2225c9be557e8b9335a630c3f3f2423bf41
Katie Durga <katie.durga@gmail.com>
2020-03-05 13:07:34 -0500

    started redirect functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from './components/Home';
-import CreateAccount from './components/CreateAccount';
+import Home from './components/pages/Home';
+import EmpCreateAccount from './components/pages/EmpCreateAccount';
+import DevCreateAccount from './components/pages/DevCreateAccount';
 import DevSimple from './components/DevSimple';
 import JobSimple from './components/JobSimple';
 
@@ -10,6 +11,8 @@ function App() {
     <Router>
       <div>
         <Route exact path="/" component={Home} />
+        <Route exact path="/dev-create-account" component={DevCreateAccount} />
+        <Route exact path="/emp-create-account" component={EmpCreateAccount} />
       </div>
     </Router>
   );
