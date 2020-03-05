370c02f527d9d19a52e9ead09e7c255c764785da
MichaelRempe <michaeljrempe@gmail.com>
2020-03-05 15:09:13 -0500

    employer model data built:

--- models/employer.js
+++ models/employer.js
@@ -1,8 +1,31 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const positionSchema = require("./position");
+
 const employerSchema = new Schema({
-    
-})
+  userName: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  location: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  posted: [{ type: Schema.Types.ObjectId, ref: positionSchema }]
+});
 
 const Employer = mongoose.model("Employer", employerSchema);
+
+module.exports = Employer;
