da4a17abe7306d0eb1b85f8cd760b92512530c44
Ken Shiraishi <kshiraishi22@gmail.com>
2020-03-06 10:09:23 -0500

    created profile page with cards

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import EmpCreateAccount from './components/pages/EmpCreateAccount';
 import DevCreateAccount from './components/pages/DevCreateAccount';
 import DevSimple from './components/DevSimple';
 import JobSimple from './components/JobSimple';
+import Profile from "./components/pages/Profile"
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/dev-create-account" component={DevCreateAccount} />
         <Route exact path="/emp-create-account" component={EmpCreateAccount} />
+        <Route exact path="/profile" component={Profile} />
       </div>
     </Router>
   );
