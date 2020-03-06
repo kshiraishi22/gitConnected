e3aba7579dcbcd6fb6b104bc36fb1491352bf555
MichaelRempe <michaeljrempe@gmail.com>
2020-03-06 12:54:13 -0500

    skeleton axios functions

---
+++ client/src/utils/API.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export default {
+    //  Position Collection
+    getAllJobs  :   function(){},
+    getJob      :   function(){},
+    updateJob   :   function(){},
+    deleteJob   :   function(){},    
+
+
+    //  Employer Collection
+    getAllDevs  :  function(){},
+    getDev      :  function(){},
+    updateDev   :  function(){},
+    deleteDev   :  function(){},   
+
+    //  Developer Collection
+    getAllEmployers :   function(){},
+    getEmployer     :   function(){},
+    updateEmployer  :   function(){},
+    deleteEmployer  :   function(){},   
+}
