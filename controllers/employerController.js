a3bbebf2719924fda7ac4db4d04261a86fa34ec1
MichaelRempe <michaeljrempe@gmail.com>
2020-03-06 10:15:03 -0500

    Model data amended for collection references

--- controllers/employerController.js
+++ controllers/employerController.js
@@ -1,7 +1,7 @@
 //Controls CRUD operations to EMPLOYER collection in mongo DB
 const db = require("../models");
 
-module.exports{
-    //Crud Ops
-    //functionName : functionCode
-}
+// module.exports{
+//     //Crud Ops
+//     //functionName : functionCode
+// }
