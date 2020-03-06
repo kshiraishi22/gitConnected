89ddfa8c6ec986c2bdd813b8b7881035f5a5c560
MichaelRempe <michaeljrempe@gmail.com>
2020-03-06 12:09:59 -0500

    employer basic CRUD ops

--- controllers/employerController.js
+++ controllers/employerController.js
@@ -1,8 +1,40 @@
 //Controls CRUD operations to EMPLOYER collection in mongo DB
 const db = require("../models");
 
-// module.exports{
-//     //Crud Ops
-//     //functionName : functionCode
-// }
-
+module.exports = {
+    //query DB for ALL employers
+    findAll: function(req, res){
+        db.Employer
+        .find()
+        .then(collection => res.json(collection))
+        .catch(err => res.status(422).json(err));
+    },
+    // query DB for SPECIFIC employer by ID (passed in via request body)
+    findById: function(req, res){
+        db.Employer
+        .findById(req.params.id)
+        .then(document => res.json(document))
+        .catch(err => res.status(422).json(err));
+    },
+    // create NEW employer, object data passed in through request body
+    create: function(req, res){
+        db.Employer
+        .create(req.body)
+        .then(document => res.json(document))
+        .catch(err => res.status(422).json(err));
+    },
+    // update SPECIFIC employer via id and update data in request body
+    update: function(req, res){
+        db.Employer
+        .findByIdAndUpdate({_id: req.params.id}, req.body)
+        .then(updatedDoc => res.json(updatedDoc))
+        .catch(err => res.status(422).json(err))
+    },
+    // delete SPECIFIC employer via id and removes data in db
+    delete: function(req, res){
+        db.Employer
+        .findByIdAndDelete({_id: req.params.id})
+        .then(deletedDoc => res.json(deletedDoc))
+        .catch(err => res.status(422).json(err))
+    }
+};
