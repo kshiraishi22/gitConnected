64794a63c82d7449bf1063dc2a046d70f2093348
Ken Shiraishi <kshiraishi22@gmail.com>
2020-03-07 13:17:38 -0500

    fixed position variable to job

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,12 +1,23 @@
-mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const db = require("../models")
+const dbConfig = require("../db/config");
 
-mongoose.connect(
-  porcess.env.MONGODB_URI ||
-  "mongodb://localhost/devNetwork_DB"
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/devNetwork_DB", { 
+  useNewUrlParser: true, 
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
 
 const jobPostings = [
+  {
+    title: "Jr Back End Developer",
+    position: "Back End",
+    empType: "Full-Time",
+    salary: 60000,
+    applicants: [],
+    company: "",
+    description: "",
+  },
   {
     title: "Jr Web Developer",
     position: "Full-Stack",
@@ -18,3 +29,10 @@ const jobPostings = [
   }
 ];
 
+db.Job.remove({}, (err, res) => {
+  if (err)
+  console.log(res)});
+
+db.Job.insertMany(jobPostings, (err, res) => {
+  console.log(res)
+})
