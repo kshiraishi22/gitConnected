e8d47e749779130918093e92f186e0dfc52bd001
MichaelRempe <michaeljrempe@gmail.com>
2020-03-09 13:13:27 -0400

    address api server route errors

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -7,24 +7,24 @@ import axios from "axios";
 // createDocument   :   function(DocumentData){return axios.post("URL||route", DocumentData);},   
 export default {
     //  Job Collection
-    getAllJobs  :   function(){return axios.get("api/job");},
-    getJob      :   function(id){return axios.get("api/job"+id);},
-    updateJob   :   function(id, updateData){return axios.put("api/job"+id, updateData);},
-    deleteJob   :   function(id){return axios.delete("api/job"+id);}, 
-    createJob   :   function(jobData){return axios.post("api/job", jobData);},   
+    getAllJobs  :   function(){return axios.get("/api/job");},
+    getJob      :   function(id){return axios.get("/api/job"+id);},
+    updateJob   :   function(id, updateData){return axios.put("/api/job"+id, updateData);},
+    deleteJob   :   function(id){return axios.delete("/api/job"+id);}, 
+    createJob   :   function(jobData){return axios.post("/api/job", jobData);},   
 
 
     //  Employer Collection
-    getAllDevs  :  function(){return axios.get("api/developer");},
-    getDev      :  function(id){return axios.get("api/developer/"+id);},
-    updateDev   :  function(id, updateData){return axios.put("api/developer"+id, updateData);},
-    deleteDev   :  function(id){return axios.delete("api/developer"+id);},
-    createDev   :  function(devData){return axios.post("api/developer", devData);},
+    getAllDevs  :  function(){return axios.get("/api/developer");},
+    getDev      :  function(id){return axios.get("/api/developer/"+id);},
+    updateDev   :  function(id, updateData){return axios.put("/api/developer"+id, updateData);},
+    deleteDev   :  function(id){return axios.delete("/api/developer"+id);},
+    createDev   :  function(devData){return axios.post("/api/developer", devData);},
 
     //  Developer Collection
-    getAllEmployers :   function(){return axios.get("api/employer");},
-    getEmployer     :   function(id){axios.get("api/employer/"+id);},
-    updateEmployer  :   function(id, updateData){axios.put("api/employer/"+id, updateData);},
-    deleteEmployer  :   function(id){axios.delete("api/employer/"+id);},
-    createEmployer  :   function(empData){axios.post("api/employer" , empData);},
+    getAllEmployers :   function(){return axios.get("/api/employer");},
+    getEmployer     :   function(id){axios.get("/api/employer/"+id);},
+    updateEmployer  :   function(id, updateData){axios.put("/api/employer/"+id, updateData);},
+    deleteEmployer  :   function(id){axios.delete("/api/employer/"+id);},
+    createEmployer  :   function(empData){axios.post("/api/employer" , empData);},
 };
