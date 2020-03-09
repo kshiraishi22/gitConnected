7a4e9c3030b09a2a41fac49294eb5a4c6c770d8c
MichaelRempe <michaeljrempe@gmail.com>
2020-03-09 14:30:19 -0400

    developer feed pushed db jobs and card job data

--- client/src/components/developer/devFeed.js
+++ client/src/components/developer/devFeed.js
@@ -1,76 +1,81 @@
 import React, { useState, useEffect } from "react";
-import { withRouter } from "react-router"
+import { withRouter } from "react-router";
 import Footer from "../Footer";
 import Navbar from "../Navbar";
 import JobCard from "./jobCard";
 import Container from "../Container";
 import Personal from "./Personal";
 import JobDetails from "./JobDetails";
-import API from "../../utils/API"
+import API from "../../utils/API";
 
 function DevFeed() {
-    
-    const [jobList, setJobList] = useState([]);
+  const [jobList, setJobList] = useState([]);
+  useEffect(() => {
+    loadJobs();
+  }, []);
 
-    useEffect(() =>{
-        loadJobs()
-    }, [])
-    
-    function loadJobs(){
-        API.getAllJobs()
-        .then(res =>{
-            console.log(res)
-        })
-    }
-    
+  function loadJobs() {
+    API.getAllJobs()
+      .then(data => setJobList(data.data))
+      .catch(err => console.log(err));
+  }
 
-        function profileSubmit(e){
-        e.preventDefault()
-        console.log("Hi")
-        const area = this
-        console.log(area)
-        // area.props.history.push("/feed/emp")
-    }
-        return (
-            <>
-            <Container>
-                <div className="row">
-                    <div className="col-12">
-                    <Navbar />
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-12">
-                    <Footer />
-                    </div>
-                </div>
-               
-              
-                <div className="row">
-                
-                <div className="col-2" style ={{backgroundColor: "blue",height: "500px", position: "relative"}}>
-                    <Personal 
-                    
-                    // profileSubmit= {profileSubmit.bind(this)}
-                    />
-                    </div>
-                
-              
-                    <div className="col-5" style= {{backgroundColor: "grey", height: "500px", overflowY:"scroll"}}>
-                        <JobCard />
-                       
-                        </div>
-                        
-                        
-                    <div className="col-5" style= {{height: "500px"}}>
-                        <JobDetails />
-                    </div>
-                    
-                </div>
-              </Container> 
-            </>
-        )
-    
+  function profileSubmit(e) {
+    e.preventDefault();
+    console.log("Hi");
+    const area = this;
+    console.log(area);
+    // area.props.history.push("/feed/emp")
+  }
+  return (
+    <>
+      <Container>
+        <div className="row">
+          <div className="col-12">
+            <Navbar />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <Footer />
+          </div>
+        </div>
+
+        <div className="row">
+          <div
+            className="col-2"
+            style={{
+              backgroundColor: "blue",
+              height: "500px",
+              position: "relative"
+            }}
+          >
+            <Personal
+
+            // profileSubmit= {profileSubmit.bind(this)}
+            />
+          </div>
+
+          <div
+            className="col-5"
+            style={{
+              backgroundColor: "grey",
+              height: "500px",
+              overflowY: "scroll"
+            }}
+          >
+              {jobList.map(job=>(
+                  <JobCard data={job} key={job._id}></JobCard>
+              ))}
+          </div>
+
+          <div className="col-5" style={{ height: "500px" }}>
+            <JobDetails />
+          </div>
+        </div>
+      </Container>
+    </>
+  );
 }
 
 export default withRouter(DevFeed);
--- client/src/components/developer/jobCard.js
+++ client/src/components/developer/jobCard.js
@@ -3,29 +3,19 @@ import JobDetails from "./JobDetails";
 import { Link, Route } from "react-router-dom";
 
 function JobCard(props) {
-    return (
-        <>
-            <div className="card" style={{marginTop: "5px"}}>
-                <div className="card-header">
-                    Company Name
-                </div>
-                <div className="card-body">
-                    <h5 className="card-title">Title</h5>
-                    <p className="card-text">Position</p>
-                    <p className="card-text">Salary</p>
-                    <p className="card-text">Location</p>
-                    {/* <Link to={`${props.match.url}jobdetails`} role="button" className="btn btn-secondary btn-block" style={{ position: "absolute", bottom: "5px" }}>
-                    Update Profile
-                    </Link>{" "}
-                    <Route exact path={`${props.match.url}jobdetails`} component={JobDetails} /> */}
-
-                    {/* <a href="#" className="btn btn-primary">Details</a> */}
-                </div>
-            </div>
-           
-            
-        </>
-    )
+  return (
+    <>
+      <div className="card" style={{ marginTop: "5px" }}>
+        <div className="card-header">Company Name</div>
+        <div className="card-body">
+          <h5 className="card-title">{props.data.title}</h5>
+          <p className="card-text">{props.data.position}</p>
+          <p className="card-text">{props.data.salary}</p>
+          <p className="card-text">{props.data.empType}</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default JobCard;
