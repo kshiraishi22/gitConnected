c30bc6e18fc60a79893c3268321b8b60eb926083
MichaelRempe <michaeljrempe@gmail.com>
2020-03-09 10:04:29 -0400

    seeded jobs

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -10,18 +10,118 @@ mongoose.connect(
   // useUnifiedTopology: true
 });
 
-const jobPostingSeed = [
+const jobSeed = [
   { 
     jobPostings: [
       {
-        title: "Jr Back End Developer",
-        position: "Back End",
+        title: "Jr. Front End Developer",
+        position: "Front End",
         empType: "Full-Time",
-        salary: 60000,
+        salary: 65000,
         applicants: [],
         company: "",
-        description: "",
-      }
+        description: "Required Technologies include: JavaScript/jQuery, CSS/Bootstrap, HTML/HandleBars.js",
+      },
+      {
+        title: "Jr. Back End Developer",
+        position: "Back End",
+        empType: "Full-Time",
+        salary: 65000,
+        applicants: [],
+        company: "",
+        description: "Required Technologies include: JavaScript, Node.js, Express.js, ",
+      },
+      {
+        title: "Jr. Full Stack Developer",
+        position: "Full Stack",
+        empType: "Full-Time",
+        salary: 75000,
+        applicants: [],
+        company: "",
+        description: "Seeking a qualified MERN developer familiar with all aspects of web development",
+      },
+      {
+        title: "Web Developer Internship",
+        position: "Front End",
+        empType: "Part Time",
+        salary: 0,
+        applicants: [],
+        company: "",
+        description: "Seeking motivated individual looking to learn more about the IT industry",
+      },
+      {
+        title: "Software Test Engineer",
+        position: "Front End",
+        empType: "Contract",
+        salary: 15000,
+        applicants: [],
+        company: "",
+        description: "Seeking a problem solver with familiarity in debugging and detail oriented note taking",
+      },
+      {
+        title: " Sr. Full Stack Developer",
+        position: "Full Stack",
+        empType: "Full-Time",
+        salary: 85000,
+        applicants: [],
+        company: "",
+        description: "Seeking skilled MEAN developer with extensive knowledge in the MEAN stack (Mongo, Express, Angular and Node.js",
+      },
+      {
+        title: "Sr. Software Engineer",
+        position: "Back End",
+        empType: "Full-Time",
+        salary: 90000,
+        applicants: [],
+        company: "",
+        description: "Seeking qualified Java engineer capable of utilizing Swing and AWS.",
+      },
+      {
+        title: "Associate Front End Developer",
+        position: "Back End",
+        empType: "Part-Time",
+        salary: 40000,
+        applicants: [],
+        company: "",
+        description: "Qualified candidates should have a flexible schedule and be willing to work remotely, contributing to numerous projects requiring skills in: React.js, BootStrap/CSS and more.",
+      },
+      {
+        title: "Associate Back End Developer",
+        position: "Back End",
+        empType: "Part-Time",
+        salary: 40000,
+        applicants: [],
+        company: "",
+        description: "Qualified candidates should have a flexible schedule and be willing to work remotely, contributing to numerous projects requiring skills in: Mongoose.js, Node.js, JavaScript/Jquery and more.",
+      },
+      {
+        title: "Sr. Front End Developer",
+        position: "Front End",
+        empType: "Full-Time",
+        salary: 115000,
+        applicants: [],
+        company: "",
+        description: "Developer will undergo portfolio screening for best practice algorithms to keep client browsers running as efficiently as possible.",
+      },
+      {
+        title: "Sr. Systems Engineer",
+        position: "Back End",
+        empType: "Full-Time",
+        salary: 125000,
+        applicants: [],
+        company: "",
+        description: "Candidates should be familiar in routing and establishing web servers for the best practices for heavily trafficked web pages with secured data",
+      },
+      {
+        title: "Sr. Full Stack Developer (Node/React.js)",
+        position: "Full Stack",
+        empType: "Full-Time",
+        salary: 130000,
+        applicants: [],
+        company: "",
+        description: "Qualified candidates will have multiple proven years of experience in the MERN stack and undergo screening on capability in various technologies, including but not limited to: MongoDB/Mongoose, NoSQL secure practices, Node.js + NPM, React.js, AWS and more. ",
+      },
+      
     ]
   }
 ];
@@ -100,7 +200,15 @@ const companySeed = [
 ]
 
 db.Job.deleteMany({})
-  .then(() => db.Job.collection.insertMany(companySeed))
+  .then(() => db.Job.collection.insertMany(jobSeed))
   .then(data => {
     console.log(data)
   });
+
+db.Employer.deleteMany({})
+  .then(()=>{db.Employer.collection.insertMany(companySeed)})
+  .then(data =>{
+    console.log(data)
+  })
+
+  //who ever seeds developer copy above code to delete anything that could exist in the developer seed (db.Developer.deleteMany) and then seed the db with your array of objects containing relevant developer info
