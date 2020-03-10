da0ac15cfd5a2a4d94b67496193dfa0e50359ef6
MichaelRempe <michaeljrempe@gmail.com>
2020-03-10 14:32:28 -0400

    dropdown updates

--- client/src/components/pages/DevCreateAccount.js
+++ client/src/components/pages/DevCreateAccount.js
@@ -1,146 +1,228 @@
 import React, { useState, useEffect } from "react";
 import API from "../../utils/API";
 
-
-const initialState = {
-  firstName: "",
-  lastName: "",
-  emailAddress: "",
-  location: "",
-  devType: "",
-  empType: "",
-  languages: "",
-  profile: "",
-  firstNameError: "",
-  lastNameError: "",
-  emailAddressError: ""
-}
-
-
 function DevCreateAccount() {
-
-  useEffect(() => {}, [])
-  const [formObject, setFormObject] = useState({});
-  const [devData, setDevData] = useState({});
+  const [formObject, setFormObject] = useState({
+    firstName:"",
+    lastName:"",
+    emailAddress:"",
+    devType:"",
+    empType:"",
+    languages:[],
+    location:"",
+    profile:"",
+  });
+  console.log(formObject);
 
   function handleInputChange(event) {
     const { name, value } = event.target;
-    setFormObject({...formObject, [name]: value})
-    console.log(formObject)
-  };
+    setFormObject({ ...formObject, [name]: value });
+    console.log(formObject);
+  }
+  function handleDropDownChange(event) {
+    const { name, value } = event.target;
+    console.log(name + "   " + value);
+    setFormObject({ ...formObject, [name]: value });
+  }
 
   function validateEmail(mail) {
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(formObject.emailAddress.value)) {
-      return (true)
+    if (
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
+        formObject.emailAddress.value
+      )
+    ) {
+      return true;
+    } else {
+      alert("You have entered an invalid email address!");
+      return false;
     }
-    else {
-      alert("You have entered an invalid email address!")
-      return (false)
-    }
-  };
-
+  }
 
   function handleFormSubmit(event) {
     event.preventDefault();
-    this.setState(initialState)
     validateEmail(formObject.emailAddress);
-    if (formObject.firstName && formObject.lastName && formObject.emailAddress) {
+    if (
+      formObject.firstName &&
+      formObject.lastName &&
+      formObject.emailAddress
+    ) {
       API.createDev({
         firstName: formObject.firstName,
-          lastName: formObject.lastName,
-          emailAddress: formObject.emailAddress,
-          location: formObject.location,
-          devType: formObject.devType,
-          empType: formObject.empType,
-          languages: formObject.languages,
-          profile: formObject.profile
-        })
+        lastName: formObject.lastName,
+        emailAddress: formObject.emailAddress,
+        location: formObject.location,
+        devType: formObject.devType,
+        empType: formObject.empType,
+        languages: formObject.languages,
+        profile: formObject.profile
+      })
         .then(() => console.log(formObject))
         .catch(err => console.log(err));
     }
-  };
+  }
 
   return (
-
-    <div className='container'>
+    <div className="container">
       <div className="jumbotron jumbotron-fluid">
         <div className="container">
           <h1 className="display-4">Hello Fellow Developer!</h1>
-          <p className="lead">Let's get started.  Fill out the following to create an account.</p>
+          <p className="lead">
+            Let's get started. Fill out the following to create an account.
+          </p>
         </div>
       </div>
-      
+
       <form>
         <div className="form-group">
-          <input onChange={handleInputChange} name="firstName" type="text" className="form-control" id="firstName" placeholder="First Name (Required)"/>
+          <input
+            onChange={handleInputChange}
+            name="firstName"
+            type="text"
+            className="form-control"
+            id="firstName"
+            placeholder="First Name (Required)"
+          />
         </div>
         <div className="form-group">
-          <input onChange={handleInputChange} name="lastName" type="text" className="form-control" id="lastName" placeholder="Last Name (Required)"/>
+          <input
+            onChange={handleInputChange}
+            name="lastName"
+            type="text"
+            className="form-control"
+            id="lastName"
+            placeholder="Last Name (Required)"
+          />
         </div>
         <div className="form-group">
           <label>Email address</label>
-          <input onChange={handleInputChange} name="emailAddress" type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com (Required)"/>
+          <input
+            onChange={handleInputChange}
+            name="emailAddress"
+            type="email"
+            className="form-control"
+            id="exampleFormControlInput1"
+            placeholder="name@example.com (Required)"
+          />
         </div>
         <div className="form-group">
           <label>Type of Developer</label>
-          <select className="form-control" id="typeofdeveloper">
-            <option onChange={handleInputChange} name="devType" value='fullStack' data-value='fullStack'>Full-Stack</option>
-            <option onChange={handleInputChange} name="devType" value='frontEnd' data-value='frontEnd'>Front End</option>
-            <option onChange={handleInputChange} name="devType" value='backEnd' data-value='backEnd'>Back End</option>
+          <select
+            className="form-control"
+            name="devType"
+            value={formObject.devType}
+            onChange={handleDropDownChange}
+          >
+            <option name="devType" value="fullStack">
+              Full-Stack
+            </option>
+            <option name="devType" value="frontEnd">
+              Front End
+            </option>
+            <option name="devType" value="backEnd">
+              Back End
+            </option>
           </select>
         </div>
         <div className="form-group">
           <label>Type of Employment</label>
-          <select className="form-control" id="typeofdeveloper">
-            <option onChange={handleInputChange} name="empType" value='fullTime' data-value='fullTime'>Full-Time</option>
-            <option onChange={handleInputChange} name="empType" value='partTime' data-value='partTime'>Part-Time</option>
-            <option onChange={handleInputChange} name="empType" value='contract' data-value='contract'>Contract</option>
-            <option onChange={handleInputChange} name="empType" value='volunteer' data-value='volunteer'>Volunteer</option>
+          <select
+            className="form-control"
+            value={formObject.empType}
+            name="empType"
+            onChange={handleDropDownChange}
+          >
+            <option name="empType" value="fullTime">
+              Full-Time
+            </option>
+            <option name="empType" value="partTime">
+              Part-Time
+            </option>
+            <option name="empType" value="contract">
+              Contract
+            </option>
+            <option name="empType" value="volunteer" >
+              Volunteer
+            </option>
           </select>
         </div>
         <div className="form-group">
           <label>Select primary languages</label>
           <div className="form-check">
-            <input onChange={handleInputChange} name="languages" className="form-check-input" type="checkbox" value="javascript" id="checkbox"/>
-            <label className="form-check-label">
-              JavaScript
-            </label>
+            <input
+              onChange={handleInputChange}
+              name="languages"
+              className="form-check-input"
+              type="checkbox"
+              value="javascript"
+              id="checkbox"
+            />
+            <label className="form-check-label">JavaScript</label>
           </div>
           <div className="form-check">
-            <input onChange={handleInputChange} name="languages" className="form-check-input" type="checkbox" value="java" id="checkbox"/>
-            <label className="form-check-label">
-              Java
-            </label>
+            <input
+              onChange={handleInputChange}
+              name="languages"
+              className="form-check-input"
+              type="checkbox"
+              value="java"
+              id="checkbox"
+            />
+            <label className="form-check-label">Java</label>
           </div>
           <div className="form-check">
-            <input onChange={handleInputChange} name="languages" className="form-check-input" type="checkbox" value="c" id="checkbox"/>
-            <label className="form-check-label">
-              C
-            </label>
+            <input
+              onChange={handleInputChange}
+              name="languages"
+              className="form-check-input"
+              type="checkbox"
+              value="c"
+              id="checkbox"
+            />
+            <label className="form-check-label">C</label>
           </div>
           <div className="form-check">
-            <input onChange={handleInputChange} name="languages" className="form-check-input" type="checkbox" value="c++" id="checkbox"/>
-            <label className="form-check-label">
-              C++
-            </label>
+            <input
+              onChange={handleInputChange}
+              name="languages"
+              className="form-check-input"
+              type="checkbox"
+              value="c++"
+              id="checkbox"
+            />
+            <label className="form-check-label">C++</label>
           </div>
           <div className="form-check">
-            <input onChange={handleInputChange} name="languages" className="form-check-input" type="checkbox" value="python" id="checkbox"/>
-            <label className="form-check-label">
-              Python
-            </label>
+            <input
+              onChange={handleInputChange}
+              name="languages"
+              className="form-check-input"
+              type="checkbox"
+              value="python"
+              id="checkbox"
+            />
+            <label className="form-check-label">Python</label>
           </div>
           <div className="form-check">
-            <input onChange={handleInputChange} name="languages" className="form-check-input" type="checkbox" value="ruby" id="checkbox"/>
-            <label className="form-check-label">
-              Ruby
-            </label>
+            <input
+              onChange={handleInputChange}
+              name="languages"
+              className="form-check-input"
+              type="checkbox"
+              value="ruby"
+              id="checkbox"
+            />
+            <label className="form-check-label">Ruby</label>
           </div>
           <div className="form-check">
-            <input onChange={handleInputChange} name="languages" className="form-check-input" type="checkbox" value="php" id="checkbox"/>
-            <label className="form-check-label">
-              PHP
-            </label>
+            <input
+              onChange={handleInputChange}
+              name="languages"
+              className="form-check-input"
+              type="checkbox"
+              value="php"
+              id="checkbox"
+            />
+            <label className="form-check-label">PHP</label>
           </div>
         </div>
         <div className="form-group">
@@ -148,9 +230,24 @@ function DevCreateAccount() {
           <textarea className="form-control" id="aboutMe" rows="3"></textarea>
         </div>
       </form>
-      <button onChange={handleInputChange} name="profile" type="button" className="btn btn-success" disabled={!(formObject.firstName && formObject.lastName && formObject.emailAddress)} onClick={handleFormSubmit}>Submit</button>
+      <button
+        onChange={handleInputChange}
+        name="profile"
+        type="button"
+        className="btn btn-success"
+        disabled={
+          !(
+            formObject.firstName &&
+            formObject.lastName &&
+            formObject.emailAddress
+          )
+        }
+        onClick={handleFormSubmit}
+      >
+        Submit
+      </button>
     </div>
-  )
-};
+  );
+}
 
 export default DevCreateAccount;
