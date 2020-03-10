74fa07f4bcaaf240f4c754f60121dd45c90099c4
MichaelRempe <michaeljrempe@gmail.com>
2020-03-10 17:14:28 -0400

    axios call and data capture

--- client/src/components/pages/EmpCreateAccount.js
+++ client/src/components/pages/EmpCreateAccount.js
@@ -1,81 +1,123 @@
 import React, { useState, useEffect } from "react";
 import API from "../../utils/API";
 
-
 function EmpCreateAccount() {
-
-  useEffect(() => {}, []);
-  const [formObject, setFormObject] = useState({});
+  const [formObject, setFormObject] = useState({
+    companyName: "",
+    email: "",
+    phone: "",
+    location: "",
+    about: ""
+  });
 
   function handleInputChange(event) {
     const { name, value } = event.target;
-    setFormObject({...formObject, [name]: value})
-    console.log(formObject)
-  };
+    setFormObject({ ...formObject, [name]: value });
+  }
 
   function validateEmail(mail) {
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
-      return (true)
+      return true;
+    } else {
+      alert("You have entered an invalid email address!");
+      return false;
     }
-    else {
-      alert("You have entered an invalid email address!")
-      return (false)
-    }
-  };
+  }
 
   function handleFormSubmit(event) {
     event.preventDefault();
-    // this.setState(initialState)
-    validateEmail(formObject.email);
-    if (formObject.companyName && formObject.email) {
-      console.log(formObject)
-      API.createEmployer(
-        {
-          companyName: formObject.companyName,
-          email: formObject.email,
-          phone: formObject.phone,
-          location: formObject.location,
-          aboutCompany: formObject.aboutCompany
-        }
-      ).catch(err => console.log(err));
-    };
-  };
+    if (!formObject.companyName && !formObject.email) {
+      alert("Required fields must be filled out!");
+      return;
+    }
+    if (validateEmail(formObject.email) === false) {
+      return;
+    }
+    API.createEmployer(formObject)
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+  }
 
   return (
-    <div className='container'>
+    <div className="container">
       <div className="jumbotron jumbotron-fluid">
         <div className="container">
           <h1 className="display-4">Let's find you a developer (or more)</h1>
-          <p className="lead">Fill out the following to register your company.</p>
+          <p className="lead">
+            Fill out the following to register your company.
+          </p>
         </div>
       </div>
-      
+
       <form>
         <div className="form-group">
-          <input onChange={handleInputChange} type="text" className="form-control" id="companyName" name='companyName' placeholder="Company Name (Required)"/>
+          <input
+            onChange={handleInputChange}
+            type="text"
+            className="form-control"
+            name="companyName"
+            placeholder="Company Name (Required)"
+          />
         </div>
         <div className="form-group">
-          <input onChange={handleInputChange} type="text" className="form-control" id="email" name='email' placeholder="Email (Required)"/>
+          <input
+            onChange={handleInputChange}
+            type="text"
+            className="form-control"
+            name="email"
+            placeholder="example@email.com (Required)"
+          />
         </div>
         <div className="form-group">
-          <input onChange={handleInputChange} type="text" className="form-control" id="phone" name='phone' placeholder="Phone Number"/>
+          <input
+            onChange={handleInputChange}
+            type="text"
+            className="form-control"
+            name="phone"
+            placeholder="Phone Number"
+          />
         </div>
         <div className="form-group">
           <label>Company Location</label>
-          <select className="form-control" id="location">
-            <option onChange={handleInputChange} name='location' data-value='West-Coast'>West Coast</option>
-            <option onChange={handleInputChange} name='location' data-value='East-Coast'>East Coast</option>
-            <option onChange={handleInputChange} name='location' data-value='Mid-West'>Midwest</option>
+          <select
+            onChange={handleInputChange}
+            className="form-control"
+            name="location"
+            value={formObject.location}
+          >
+            <option name="location" value="West-Coast">
+              West Coast
+            </option>
+            <option name="location" value="East-Coast">
+              East Coast
+            </option>
+            <option name="location" value="MidWest">
+              Midwest
+            </option>
           </select>
         </div>
         <div className="form-group">
           <label>Tell us a little bit about your company</label>
-          <textarea onChange={handleInputChange} name='aboutCompany' className="form-control" id="aboutCompany" rows="3"></textarea>
+          <textarea
+            onChange={handleInputChange}
+            name="about"
+            className="form-control"
+            rows="3"
+          ></textarea>
         </div>
       </form>
-      <button onChange={handleInputChange} name="profile" type="button" className="btn btn-success" disabled={!(formObject.companyName && formObject.email)} onClick={handleFormSubmit}>Submit</button>
+      <button
+        // onChange={handleInputChange}
+        name="profile"
+        type="button"
+        className="btn btn-success"
+        disabled={!(formObject.companyName && formObject.email)}
+        onClick={handleFormSubmit}
+      >
+        Submit
+      </button>
     </div>
-  )
-};
+  );
+}
 
 export default EmpCreateAccount;
