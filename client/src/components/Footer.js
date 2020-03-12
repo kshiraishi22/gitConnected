3cb604885e0e3c09a93974a294414a118f1ece14
Khalil <khalilhall1221@gmail.com>
2020-03-12 00:31:34 -0400

    nav

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer(){
     return(
         <nav class="navbar fixed-bottom navbar-dark bg-dark" style={{position: "fixed", marginTop: "0"}}>
-        <a class="navbar-brand" href="#">Fixed bottom</a>
+        <a class="navbar-brand" href="#"></a>
         </nav>
     )
 }
