0d48b00e803a26b3eefa0f199dda6821429c28ef
Katie Durga <katie.durga@gmail.com>
2020-03-25 21:43:13 -0400

    adding stuff

---
+++ client/src/firebase-config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBP3PcpfqQqujEINekhS5RdZAx7Rgvj6Qo",
+  authDomain: "gitconnected-b3faf.firebaseapp.com",
+  databaseURL: "https://gitconnected-b3faf.firebaseio.com",
+  projectId: "gitconnected-b3faf",
+  storageBucket: "gitconnected-b3faf.appspot.com",
+  messagingSenderId: "919547732287",
+  appId: "1:919547732287:web:dbc1d30f912ffb98bf338a",
+  measurementId: "G-VW4HK6ZWNY"
+};
+export default firebaseConfig;
